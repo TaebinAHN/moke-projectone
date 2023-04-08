f675e87e1e0871a86be3dc826df4e95ce4c93543
태빈 안 <65588496+TaebinAHN@users.noreply.github.com>
2023-04-08 23:08:39 +0900

    oracle연결설정 추가

--- src/test/java/moke/mokeprojectone/MokeProjectoneApplicationTests.java
+++ src/test/java/moke/mokeprojectone/MokeProjectoneApplicationTests.java
@@ -1,13 +1,28 @@
 package moke.mokeprojectone;
 
+import lombok.extern.slf4j.Slf4j;
+import moke.mokeprojectone.mapper.TimeMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class MokeProjectoneApplicationTests {
 
+	@Autowired
+	private TimeMapper timeMapper;
+
 	@Test
-	void contextLoads() {
+	public void testGetTime(){
+		log.info("timeMapper Class name : " + timeMapper.getClass().getName());
+		log.info("timeMapper time : " + timeMapper.getTime());
+	}
+
+	@Test
+	public void testGetTime2(){
+		log.info("getTime2");
+		log.info("getTime2: " + timeMapper.getTime2());
 	}
 
 }
